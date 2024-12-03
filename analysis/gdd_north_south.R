9e7a2d4466fff85e299bf6b159705a6ecbfd6459
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2024-12-03 17:05:25 +0100

    Quantile definition

--- analysis/gdd_north_south.R
+++ analysis/gdd_north_south.R
@@ -92,4 +92,51 @@ lower_plot <- ggplot(data = optimality_historical, aes(x = doy)) +
   scale_y_continuous("Optimality") +
   coord_cartesian(xlim = c(0,365), ylim = c(0.4,1.1), expand = FALSE)
 
-upper_plot + lower_plot + plot_layout(ncol = 1)
+cowplot::ggsave2(filename = file.path(wd, "analysis", "optimality_examples.pdf"),
+                 plot = upper_plot + lower_plot + plot_layout(ncol = 1), 
+                 device = cairo_pdf, width = 130, height = 100, unit = "mm")
+
+optimality_historical <- optimality_historical %>%
+  group_by(id) %>%
+  mutate(
+    qt975 = quantile(opt, 0.975), 
+    qt95 = quantile(opt, 0.95), 
+    qt90 = quantile(opt, 0.90))
+
+quantile_def_plot <- ggplot(data = optimality_historical, aes(x = doy)) + 
+  facet_grid(~ point) +
+  geom_line(aes(y = opt), 
+            linewidth = 2, color = "white") +
+  geom_line(aes(y = opt), 
+            linewidth = 0.8, color = "grey") +
+  geom_hline(aes(yintercept = qt975), data = unique(optimality_historical[c("qt975", "point")]), linetype = "dashed", linewidth = 0.5, color = "#441752") +
+  geom_hline(aes(yintercept = qt95), data = unique(optimality_historical[c("qt95", "point")]), linetype = "dashed", linewidth = 0.5, color = "#AB4459") +
+  geom_hline(aes(yintercept = qt90), data = unique(optimality_historical[c("qt90", "point")]), linetype = "dashed", linewidth = 0.5, color = "#F29F58") +
+  geom_boxplot(aes(x = doy, y = 0.96),
+               width = 0.03, linewidth = 0.3, outliers = FALSE, color = "#441752",
+               data = optimality_historical %>% group_by(id) %>% dplyr::filter(opt > qt975)) +
+  geom_boxplot(aes(x = doy, y = 1),
+               width = 0.03, linewidth = 0.3, outliers = FALSE, color = "#AB4459",
+               data = optimality_historical %>% group_by(id) %>% dplyr::filter(opt > qt95)) +
+  geom_boxplot(aes(x = doy, y = 1.04),
+               width = 0.03, linewidth = 0.3, outliers = FALSE, color = "#F29F58",
+               data = optimality_historical %>% group_by(id) %>% dplyr::filter(opt > qt90)) +
+  geom_text(data = data.frame(x = 205, y = c(0.96), label = c("Q97.5"), point = "South"), aes(x, y, label = label),
+            size = 2, color = "#441752") +
+  geom_text(data = data.frame(x = 205, y = c(1), label = c("Q95"), point = "South"), aes(x, y, label = label),
+            size = 2, color = "#AB4459") +
+  geom_text(data = data.frame(x = 205, y = c(1.04), label = c("Q90"), point = "South"), aes(x, y, label = label),
+            size = 2, color = "#F29F58") +
+  theme_bw() +
+  theme(legend.position = 'none', panel.grid = element_blank(),
+        strip.background = element_blank(), 
+        axis.title.x = element_text(size = 8), axis.title.y = element_text(size = 9),
+        axis.title.y.left = element_text(color = "grey"),
+        strip.text.x = element_text(color = "black"), plot.margin = margin(t = 0, b = 0, l = 2, r = 2)) +
+  labs(x= "DOY") +
+  scale_y_continuous("Optimality") +
+  coord_cartesian(xlim = c(0,365), ylim = c(0.4,1.1), expand = FALSE)
+
+cowplot::ggsave2(filename = file.path(wd, "analysis", "quantile_def_plots.pdf"),
+                 plot = quantile_def_plot, 
+                 device = cairo_pdf, width = 130, height = 60, unit = "mm")
