eda3754975d3170cebf8063eb607e15d0b4dba47
Victor Van der Meersch <victor.vandermeersch@cefe.cnrs.fr>
2024-12-17 22:47:20 +0100

    making the same map for future... could be interesting?

--- figures/supp/figure_global_optimality_future.R
+++ figures/supp/figure_global_optimality_future.R
@@ -7,12 +7,14 @@ wd <- "C:/Users/vandermeersch/Documents/CEFE/projects/synchrony"
 source(file.path(wd, "scripts", "preamble.R"))
 
 models <- c("GFDL-ESM4", "IPSL-CM6A-LR", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL")
-optimality_future_ssp2 <- lapply(models, function(m) readRDS(file.path(wd, "data/processed/cmip6", "2071_2100", paste0("optimality_","ssp245","_",m,".rds"))))
+optimality_future_ssp2 <- lapply(models, function(m) readRDS(file.path(wd, "data/processed/cmip6",  paste0("optimality_","ssp245","_",m,".rds"))))
 optimality_future_ssp2 <- as.data.frame(do.call(rbind, optimality_future_ssp2))
-optimality_future_ssp5 <- lapply(models, function(m) readRDS(file.path(wd, "data/processed/cmip6", "2071_2100", paste0("optimality_","ssp585","_",m,".rds"))))
+optimality_future_ssp2$ssp <-"SSP2-4.5"
+optimality_future_ssp5 <- lapply(models, function(m) readRDS(file.path(wd, "data/processed/cmip6",  paste0("optimality_","ssp585","_",m,".rds"))))
 optimality_future_ssp5 <- as.data.frame(do.call(rbind, optimality_future_ssp5))
+optimality_future_ssp5$ssp <-"SSP5-8.5"
 optimality_future <- rbind(optimality_future_ssp2, optimality_future_ssp5)
-optimality_future$ssp <- ifelse(optimality_future$ssp == "ssp245", "SSP2-4.5", "SSP5-8.5")
+
 
 global_optimum <- optimality_future %>% 
   group_by(ssp, period, doy) %>%
@@ -39,15 +41,15 @@ optimum_plot <- ggplot() +
                data = local_optima %>% filter(period == "2071_2100")) +
   geom_line(aes(y = opt, x = doy, group = paste0(gcm, ssp)), 
             data = global_optimum_pergcm %>% filter(period == "2071_2100"),
-            alpha = 0.3, color = "#17a353",
+            alpha = 0.3, color = "#f59c10",
             linewidth = 0.2, lineend = "round") +
   geom_line(aes(y = opt, x = doy, group = 1), 
             data = global_optimum  %>% filter(period == "2071_2100"), color = "white",
             linewidth = 1.1, lineend = "round") +
-  geom_line(aes(y = opt, x = doy, color = paste0(ssp,opt_period), group = 1), 
+  geom_line(aes(y = opt, x = doy, color = opt_period, group = 1), 
             data = global_optimum %>% filter(period == "2071_2100"),
             linewidth = 0.6, lineend = "round") +
-  scale_color_manual(values = c("#17a353", "#c1121f", "#17a353", "#c1121f")) +
+  scale_color_manual(values = c("#f59c10", "#c1121f")) +
   theme_bw() +
   theme(legend.position = 'none', panel.grid = element_blank(),
         strip.background = element_blank(), 
---
+++ figures/supp/figure_local_optimality_future.R
@@ -0,0 +1,62 @@
+
+models <- c("GFDL-ESM4", "IPSL-CM6A-LR", "MPI-ESM1-2-HR", "MRI-ESM2-0", "UKESM1-0-LL")
+optimality_future_ssp2 <- lapply(models, function(m) readRDS(file.path(wd, "data/processed/cmip6",  paste0("optimality_","ssp245","_",m,".rds"))))
+optimality_future_ssp2 <- as.data.frame(do.call(rbind, optimality_future_ssp2))
+optimality_future_ssp2$ssp <-"SSP2-4.5"
+optimality_future_ssp5 <- lapply(models, function(m) readRDS(file.path(wd, "data/processed/cmip6",  paste0("optimality_","ssp585","_",m,".rds"))))
+optimality_future_ssp5 <- as.data.frame(do.call(rbind, optimality_future_ssp5))
+optimality_future_ssp5$ssp <-"SSP5-8.5"
+optimality_future <- rbind(optimality_future_ssp2, optimality_future_ssp5)
+
+local_optima <- optimality_future %>%
+  group_by(id) %>%
+  mutate(qt = quantile(opt, 0.90), opt_period = opt > qt, 
+         optdoy = median(doy[opt_period]), deltaopt = optdoy-172,
+         deltaopt = if_else(deltaopt > 20, 20, if_else(deltaopt < -20, -20, deltaopt)))
+
+sites <- readRDS(file.path(wd, "data/processed", "sites.rds"))
+sites <- as.data.frame(sites, geom = "XY") %>%
+  left_join(unique(local_optima[c("id", "deltaopt")]), join_by(id)) %>%
+  vect(geom = c("x", "y"))
+crs(sites) <- "EPSG:4326"
+sites_df <- as.data.frame(sites, geom = "XY")
+
+
+map <- ggplot() +
+  # tidyterra::geom_spatraster(data = mask_r %>% project("EPSG:3035")) +
+  # scale_fill_gradient(low = "grey50", high = "grey45", na.value = "transparent", guide = FALSE) +
+  tidyterra::geom_spatvector(data = aggregate(eu_map) %>% crop(ext(mask_r)) %>% project("EPSG:3035"), fill = "white",
+                             linewidth = 0.1, color = "grey60") +
+  tidyterra::geom_spatvector(data = sites %>% project("EPSG:3035"), 
+                             color = "white", size = 1.1) +
+  tidyterra::geom_spatvector(data = sites %>% project("EPSG:3035"), 
+                             aes(color = deltaopt),
+                             size = 0.7) +
+  scale_color_gradientn(colors = kippenberger, breaks = seq(-20, 20, 20), limits = c(-20,20),
+                        labels = c(paste0("\u2264\u2212","20"),  "0", paste0("\u2265","20")),
+                        name = "Optimal timing (relative to solstice)") +
+  # scale_color_viridis_c(direction = -1, breaks = seq(-20, 20, 20), option = "D",
+  #                      labels = c(paste0("\u2264\u2212","20"),  "0", paste0("\u2265","20")),
+  #                      name = "Optimal timing (relative to solstice)") +
+  # scale_color_gradient2(low = "#d95f02", mid = "#1b9e77", high = "#7570b3",
+  #                       breaks = seq(-20, 20, 20), 
+  #                       labels = c(paste0("\u2264\u2212","20"),  "0", paste0("\u2265","20")),
+  #                       name = "Optimal timing (relative to solstice)") +
+  # scale_color_manual(values = c("#1b9e77", "#d95f02", "#7570b3")) +
+  theme_void() + theme(
+    legend.position = "inside",
+    legend.position.inside =c(0.2,.8),
+    legend.direction="horizontal",
+    legend.title = element_text(size = 7, color = "grey20"),
+    plot.margin = margin(t = 0, b = 0, l = 0, r = 0))+
+  guides(
+    color = guide_colorbar(order = 1,
+                           frame.colour = "grey30", ticks.colour = NA,
+                           frame.linewidth = 0.2,
+                           title.position="top", title.hjust = 0.5,
+                           theme = theme(legend.key.height  = unit(3, "pt"),
+                                         legend.key.width  = unit(80, "pt"),
+                                         legend.text = element_text(size = 7, 
+                                                                    margin = margin(t = 3.5), color = "grey20"))))
+
+
